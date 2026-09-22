b729efb654cbe55c7ef79f387d506234b085fd7a
Kunal Kundnani <kunalkundnani726@gmail.com>
2026-09-22 15:50:53 +0530

    First commit - Mac dock added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.scss'
+import Doc from './components/Dock'
+
+const App = () => {
+  return (
+      <main>
+        <Doc/>
+      </main>
+  )
+}
+
+export default App
